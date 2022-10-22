8cdb7337df680a786ee4c30989a5f64eae9e48f7
olgazinchenko <ozinchenko.pm@gmail.com>
2022-10-22 10:39:49 +0200

    Initial Commit

---
+++ BabySleepDiary/BabySleepDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySleepDiaryApp.swift
+//  BabySleepDiary
+//
+//  Created by ozinchenko.dev on 22.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BabySleepDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
