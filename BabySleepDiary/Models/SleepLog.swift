34fc44810e18faddd165517f08788b5007b9352b
olgazinchenko <ozinchenko.pm@gmail.com>
2023-01-17 21:04:18 +0100

    Updated SleepLog data and created SleepCardView

--- BabySleepDiary/Models/SleepLog.swift
+++ BabySleepDiary/Models/SleepLog.swift
@@ -12,14 +12,17 @@ struct SleepLog {
     var startTime: String
     var endTime: String
     var isNithtSleep: String
+    var icon: String
+    var theme: Theme
     var sleepDuration: String
     var wakefulnessuration: String
+    
 }
 
 extension SleepLog {
     static let sleeps: [SleepLog] =
     [
-        SleepLog(startTime: "22:08", endTime: "08:00", isNithtSleep: "Night", sleepDuration: "9 hours 52 minutes", wakefulnessuration: "5 hours 59 minutes"),
-        SleepLog(startTime: "13:59", endTime: "16:40", isNithtSleep: "Day", sleepDuration: "5 hours 59 minutes", wakefulnessuration: "5 hours 3 minutes")
+        SleepLog(startTime: "22:08", endTime: "08:00", isNithtSleep: "Night", icon: "moon.stars.fill", theme: Theme.sky, sleepDuration: "9 hours 52 minutes", wakefulnessuration: "5 hours 59 minutes"),
+        SleepLog(startTime: "13:59", endTime: "16:40", isNithtSleep: "Day", icon: "sun.max.fill", theme: Theme.happy, sleepDuration: "5 hours 59 minutes", wakefulnessuration: "5 hours 3 minutes")
     ]
 }
---
+++ BabySleepDiary/Views/SleepCardView.swift
@@ -0,0 +1,46 @@
+//
+//  SleepCardView.swift
+//  BabySleepDiary
+//
+//  Created by ozinchenko.dev on 17/01/2023.
+//
+
+import SwiftUI
+
+struct SleepCardView: View {
+    let sleep: SleepLog
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(sleep.endTime)
+            }
+            Spacer()
+            HStack(alignment: .center) {
+                HStack(alignment: .center) {
+                    Label("", systemImage: sleep.icon)
+                        .padding(.leading, 2)
+                        .font(.system(size: 24))
+                }
+                VStack(alignment: .leading) {
+                    Text("Day sleep:")
+                    Text(sleep.sleepDuration)
+                        .font(.headline)
+
+                }
+            }
+            Spacer()
+            HStack {
+                Text(sleep.startTime)
+            }
+        }
+    }
+}
+
+struct SleepCardView_Previews: PreviewProvider {
+    static var sleep = SleepLog.sleeps[0]
+    static var previews: some View {
+        SleepCardView(sleep: sleep)
+            .background(sleep.theme.mainColor)
+            .previewLayout(.fixed(width: 400, height: 80))
+    }
+}
