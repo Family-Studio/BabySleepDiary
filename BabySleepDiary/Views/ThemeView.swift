de7e0e904674f488f7fecffe3e3389647cdd1ffb
olgazinchenko <ozinchenko.pm@gmail.com>
2023-03-19 17:57:55 +0100

    Created a new ThemeView and added a theme constant and passed a value in the preview.

---
+++ BabySleepDiary/Views/ThemeView.swift
@@ -0,0 +1,21 @@
+//
+//  ThemeView.swift
+//  BabySleepDiary
+//
+//  Created by ozinchenko.dev on 19/03/2023.
+//
+
+import SwiftUI
+
+struct ThemeView: View {
+    let theme: Theme
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView(theme: .bubblegum)
+    }
+}
