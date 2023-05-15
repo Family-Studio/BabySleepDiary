3ec1561f857c4947afe7b08146e58e420b2dd834
olgazinchenko <ozinchenko.pm@gmail.com>
2023-05-15 16:11:49 +0200

    Added a method to load data

--- BabySleepDiary/Models/SleepStore.swift
+++ BabySleepDiary/Models/SleepStore.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 
+@MainActor
 class SleepStore: ObservableObject {
     @Published var sleeps: [DailySleep] = []
     
@@ -15,4 +16,16 @@ class SleepStore: ObservableObject {
             .appendingPathComponent("sleeps.data")
     }
     
+    func load() async throws {
+        let task = Task<[DailySleep], Error> {
+            let fileURL = try Self.fileURL()
+            guard let data = try? Data(contentsOf: fileURL) else {
+                return []
+            }
+            let dailySleeps = try JSONDecoder().decode([DailySleep].self, from: data)
+            return dailySleeps
+        }
+        let sleeps = try await task.value
+        self.sleeps = sleeps
+    }
 }
