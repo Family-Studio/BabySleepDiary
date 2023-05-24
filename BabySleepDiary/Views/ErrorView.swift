74761b527a7f31b18bee5ab6110b4de99d1bdd76
olgazinchenko <ozinchenko.pm@gmail.com>
2023-05-24 16:18:13 +0200

    Created an error view.

---
+++ BabySleepDiary/Views/ErrorView.swift
@@ -0,0 +1,43 @@
+//
+//  ErrorView.swift
+//  BabySleepDiary
+//
+//  Created by ozinchenko.dev on 24/05/2023.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
