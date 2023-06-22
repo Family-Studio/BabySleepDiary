7781847b23ce688658dd8c5b1a08d21e16a7d790
olgazinchenko <ozinchenko.pm@gmail.com>
2023-06-22 17:14:37 +0200

    Updated the SleepEditView.

--- BabySleepDiary/Views/SleepEditView.swift
+++ BabySleepDiary/Views/SleepEditView.swift
@@ -10,18 +10,43 @@ import SwiftUI
 struct SleepEditView: View {
     @EnvironmentObject var realmManager: RealmManager
     @State private var isNight: Bool = false
-    @State private var startTime: Date?
+    @State private var startTime = Date()
     @State private var endTime: Date?
-    
+    var closedRange: ClosedRange<Date> {
+        let currentDate = Calendar.current.date(byAdding: .day, value: 0, to: Date())!
+        let monthAgo = Calendar.current.date(byAdding: .day, value: -30, to: Date())!
+        return monthAgo...currentDate
+    }
     var body: some View {
         Form {
             Section(header: Text("Edit sleep")) {
-                Toggle(isOn: $isNight) {
-                    if isNight {
-                        Label("Night", systemImage: "moon.stars")
-                    } else {
-                        Label("Day", systemImage: "sun.max")
+                HStack {
+                    Text("Start time: ")
+                    Spacer()
+                    DatePicker("", selection: $startTime, in: closedRange, displayedComponents: .hourAndMinute)
+                    DatePicker("", selection: $startTime, in: closedRange, displayedComponents: .date)
+                        .labelsHidden()
+                        .pickerStyle(.wheel)
+                }
+                HStack {
+                    Text("End time: ")
+                    Spacer()
+                    DatePicker("", selection: $startTime, in: closedRange, displayedComponents: .hourAndMinute)
+                    DatePicker("", selection: $startTime, in: closedRange, displayedComponents: .date)
+                        .labelsHidden()
+                        .pickerStyle(.wheel)
+                }
+                HStack {
+                    Text("Sleep period:")
+                    Spacer()
+                    Toggle(isOn: $isNight) {
+                        if isNight {
+                            Label("Night", systemImage: "moon.stars")
+                        } else {
+                            Label("Day", systemImage: "sun.max")
+                        }
                     }
+                    .toggleStyle(.button)
                 }
             }
         }
