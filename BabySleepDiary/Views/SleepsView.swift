3eb8e318d536aeff4a10d3808ab7cdbcf722c143
olgazinchenko <ozinchenko.pm@gmail.com>
2023-07-14 12:03:47 +0200

    Added NavigationStak and NavigationLink to the SleepsView.

--- BabySleepDiary/Views/SleepsView.swift
+++ BabySleepDiary/Views/SleepsView.swift
@@ -11,31 +11,36 @@ struct SleepsView: View {
     @StateObject var realmManager = RealmManager()
     @Environment(\.scenePhase) private var scenePhase
     
+    
     var body: some View {
-        VStack() {
-            SleepHeaderView()
-                .environmentObject(realmManager)
-            Spacer()
-            List {
-                ForEach(realmManager.sleeps, id: \.id) { sleep in
-                    if !sleep.isInvalidated {
-                        SleepCardView(isNight: sleep.isNight, startTime: sleep.startTime ?? .now, endTime: sleep.endTime ?? .now)
-                            .swipeActions(edge: .trailing) {
-                                Button(role: .destructive) {
-                                    realmManager.deleteSleep(id: sleep.id)
-                                } label: {
-                                    Label("Delete", systemImage: "trash")
+        NavigationStack {
+            VStack() {
+                SleepHeaderView()
+                    .environmentObject(realmManager)
+                Spacer()
+                List {
+                    ForEach(realmManager.sleeps, id: \.id) { sleep in
+                        if !sleep.isInvalidated {
+                            NavigationLink(destination: SleepFooterView()) {
+                                SleepCardView(isNight: sleep.isNight, startTime: sleep.startTime ?? .now, endTime: sleep.endTime ?? .now)
+                                    .swipeActions(edge: .trailing) {
+                                        Button(role: .destructive) {
+                                            realmManager.deleteSleep(id: sleep.id)
+                                        } label: {
+                                            Label("Delete", systemImage: "trash")
 
-                                }
+                                        }
+                                    }
                             }
+                        }
                     }
                 }
+                SleepFooterView()
+                    .environmentObject(realmManager)
             }
-            SleepFooterView()
-                .environmentObject(realmManager)
-        }
-        .background(.white)
+            .background(.white)
         .scrollContentBackground(.hidden)
+        }
     }
 }
 
